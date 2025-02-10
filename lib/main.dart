af685b8f56a65a730e0b8b30f4fa6ccf2ddf4f3f
Mon143 <mon.firstbostonsoftware@gmail.com>
2025-02-10 17:55:08 +0800

    Bug Fixed Sound

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ Future<void> main() async {
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "FLY ME TO THE SPACE THE MATH ADVENTURE",
-      initialRoute: AppPages.ADDITION,
+      initialRoute: AppPages.SPLASH,
       getPages: AppPages.routes,
     ),
   );
